50731761dd416de3f028144bd52218e422e8e3bf
niefeng <v10258@qq.com>
2017-10-31 16:39:01 +0800

    用户注册登录切换功能提交

--- src/login/login.js
+++ src/login/login.js
@@ -9,11 +9,33 @@ import Vue from 'vue/dist/vue.js';
 var app = new Vue({
   el: '#loginForm',
   data: {
+  	type: 1,
+  	loginType: 1,
     isShowAskMore: false,
     message: 'Hello Vue!'
   },
+
+  created() {
+  	var self = this;
+    //获取备用数据
+    self.type = this.queryString('type') || 0;
+  },
+
+  watch: {
+    type() {
+      console.log('type', this.type);
+    },
+
+    loginType() {
+    	console.log('loginType', this.loginType);
+    }
+  },
+
   methods: {
-      askMoreToggle: ()=>{}
+    queryString: (item)=>{
+		  var svalue = location.search.match(new RegExp('[?&]' + item + '=([^&]*)(&?)', 'i'));
+		  return svalue ? svalue[1] : '';
+		}
   }
 })
 
