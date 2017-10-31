2b44cb5ee5d12ade31f068441a6af0f6ddabef4d
niefeng <v10258@qq.com>
2017-11-01 01:42:27 +0800

    调整前端运行目录

---
+++ demo/app.js
@@ -0,0 +1,68 @@
+/*!
+ * blog-mex.js
+ * @date 2015.12.6
+ * @author chenjinghui@meizu.com
+ * @version 0.0.1
+ */
+
+var path = require('path');
+
+var express = require('express');
+var nunjucks = require('nunjucks');
+var app = express();
+
+// 设置静态文件目录
+app.use(express.static(__dirname + '/../public'));
+
+nunjucks.configure(__dirname + '/views', {
+  autoescape: true,
+  express: app,
+  tags: {
+    blockStart: '<%',
+    blockEnd: '%>',
+    variableStart: '<$',
+    variableEnd: '$>',
+    commentStart: '<#',
+    commentEnd: '#>'
+  }
+});
+
+
+app.get('/', function(req, res) {
+  res.render('index.html');
+});
+
+app.get('/find', function(req, res) {
+  res.render('find.html');
+});
+
+app.get('/contact', function(req, res) {
+  res.render('contact.html');
+});
+
+app.get('/user', function(req, res) {
+  res.render('user.html');
+});
+
+app.get('/login', function(req, res) {
+  res.render('login.html');
+});
+
+app.get('/ask', function(req, res) {
+  res.render('ask.html');
+});
+
+app.get('/centre', function(req, res) {
+  res.render('centre.html');
+});
+
+app.get('/task/:id', function(req, res) {
+  res.render('task.html');
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address || 'localhost';
+  var port = server.address().port;
+
+  console.log(`Example app listening at http://${host}:${port}`);
+});
