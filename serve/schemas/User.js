a5d248d977b008fc69719acf04ecf22434469dba
youmoo <youmoolee@gmail.com>
2017-11-01 11:01:16 +0800

    初建用户时，密码也不是必须的了

--- serve/schemas/User.js
+++ serve/schemas/User.js
@@ -8,7 +8,7 @@ const schema = new Schema({
     name: {type: String},// 用户名
     phone: {type: String, required: true, unique: true},// 联系电话
     email: {type: String, unique: true},// 邮箱
-    password: {type: String, required: true},// 密码
+    password: {type: String},// 密码
     signature: {type: String},// 个性签名
     created: {type: Date},// 创建时间
     avatar: {type: String},// 用户头像
