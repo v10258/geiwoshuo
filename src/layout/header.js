dc78dfc68297b58745d3c79b845772fe0b8b7b9b
niefeng <v10258@qq.com>
2017-11-03 15:24:11 +0800

    点击页面其他位置，关闭城市选择浮层

--- src/layout/header.js
+++ src/layout/header.js
@@ -14,6 +14,7 @@ var $collect = {
   cityList: $('#J_citySwitch').next()
 }
 
+// 点击切换，显示城市选择菜单
 $collect.citySwitch.on('click', (ev)=>{
   var $elem = $(ev.currentTarget);
   var $parent = $elem.parent();
@@ -22,8 +23,10 @@ $collect.citySwitch.on('click', (ev)=>{
   } else {
     $parent.addClass('active');
   }
+  ev.stopPropagation();
 });
 
+// 点选城市，传播事件
 $collect.cityList.on('click', 'a', (ev)=>{
   var $elem = $(ev.currentTarget);
   var sid = $elem.data('sid');
@@ -31,6 +34,12 @@ $collect.cityList.on('click', 'a', (ev)=>{
   $elem.parents('.dropdown').removeClass('active');
 });
 
+// 点击页面其他位置，关闭城市切换浮层
+$(document).on('click', (ev)=>{
+  if ($(ev.target).parents('.-city').length === 0) {
+    $collect.citySwitch.parent().removeClass('active');
+  }
+});
 pageEvent.$on(pageEvent.header.citySwitch, (data)=>{
   console.log('trigger ' + pageEvent.header.citySwitch, data);
 })
