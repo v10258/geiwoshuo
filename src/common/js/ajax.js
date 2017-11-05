50409a60a70da01bf6d63c738bf125b07074815c
niefeng <v10258@qq.com>
2017-11-05 18:56:45 +0800

    添加mockjs 模拟请求接口数据

---
+++ src/common/js/ajax.js
@@ -0,0 +1,6 @@
+
+export const REMOTE = {
+    index: {
+        queryQuestions: '/api/queryQuestions'
+    }
+}
---
+++ src/common/js/mock/mock.js
@@ -0,0 +1,23 @@
+var Mock = require('mockjs');
+import {REMOTE} from '../ajax.js';
+
+Mock.mock(new RegExp(REMOTE.index.queryQuestions), {
+    "success": true,
+    "code": 200,
+    "message": '',
+    "data": {
+        "list|20": [{
+            "id|+1": 1,
+            "title|2-7": "兑换外币",
+            "body|5 - 20": "详细描述",
+            "tags|2-5":[{
+                "name|1": ['换汇','代购','投票','陪玩'],
+                "id|+1": 1
+            }],
+            "last_modified": "@datetime('yyyy-MM-dd HH:mm')",
+            "upvotes|0-100": 1,
+            "views|100-999": 1,
+            "dynamic|50-200": 1
+        }]
+    }
+});
