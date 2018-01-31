b6a884b5fdcab22d6d30fcbcc1a4b4bfe96a3054
niefeng <v10258@qq.com>
2018-01-31 22:37:20 +0800

    ask页简化

--- src/common/component/input-tag.vue
+++ src/common/component/input-tag.vue
@@ -1,23 +1,29 @@
 
 <template>
 
-<div class="ui-inputtag" @click="focusNewTag()" v-bind:class="{'read-only': readOnly}">
-  <span class="ui-inputtag-item" v-for="(tag, index) in tags" v-bind:key="index">
-    <em>{{ tag.name }}</em>
-    <a v-if="!readOnly" @click.prevent.stop="remove(index)" class="remove"></a>
-  </span>
-  <input class="ui-inputtag-new" v-if="!readOnly" :disabled="tagMax" :placeholder="tips" type="text" v-model="newTag" v-on:input="autoComplete" v-on:keydown.down="selectDown" v-on:keydown.up="selectUp" v-on:keydown.delete.stop="removeLastTag()" v-on:keydown.enter.188.tab.prevent.stop="addTagActive"/>
-  <button class="ui-inputtag-add ion ion-ios-search" @click="autoComplete"></button>
-  <div class="ui-inputtag-search" v-bind:style="{ width: autoWidth + 'px' }" v-if="newTag && active">
-    <a v-for="(tag, index) in autoTags" v-bind:class="{ active: index === 0 }" v-on:mouseenter="searchItemEnter" @click="addTag(tag)" href="javascript:;">
-    {{tag.name}}
+<div class="ui-inputtag" v-bind:class="{'read-only': readOnly}">
+  <template v-if="defaultTags && defaultTags.length">
+    <a v-for="(tag, index) in defaultTags" :key="tag.id" @click="toggleTag(tag, index)" :class="['ui-inputtag-item', tag.checked ? 'selected' : '']">
+      {{tag.name}}
     </a>
-    <a v-bind:class="{ active: autoTags.length === 0 }" v-if="!isMatch" data-new="true" @click.prevent.stop="addTag(newTag, true)" v-on:mouseenter="searchItemEnter" href="javascript:;">
-      创建
-      <strong>{{newTag}}</strong>
-      标签
-    </a>
+  </template>
+  <div class="ui-inputtag-search">
+    <input class="ui-inputtag-new" v-if="!readOnly" :disabled="tagMax" :placeholder="placeholder" type="text" v-model="newTag" v-on:input="autoComplete" v-on:keydown.down="selectDown" v-on:keydown.up="selectUp" v-on:keydown.delete.stop="removeLastTag()" v-on:keydown.enter.188.tab.prevent.stop="addTagActive"/>
+    <button class="ui-inputtag-add ion ion-ios-search" @click="autoComplete"></button>
+    <div class="ui-inputtag-result" v-if="newTag && active">
+      <a v-for="(tag, index) in autoTags" v-bind:class="{ active: index === 0 }" v-on:mouseenter="searchItemEnter" @click="addTagItem(tag)" href="javascript:;">
+      {{tag.name}}
+      </a>
+      <a v-bind:class="{ active: autoTags.length === 0 }" v-if="!isMatch" data-new="true" @click.prevent.stop="addTagItem(newTag, true)" v-on:mouseenter="searchItemEnter" href="javascript:;">
+        创建
+        <strong>{{newTag}}</strong>
+        标签
+      </a>
+    </div>
   </div>
+  <span class="ui-inputtag-mark" v-if="tagMax">
+    (最多选择{{limit}}个标签)
+  </span>
   <input type="hidden" :name="nameAndId" :id="nameAndId" :value="tagsVal">
 </div>
 
@@ -41,6 +47,10 @@
     name: 'InputTag',
 
     props: {
+      hotTags: {
+        type: Array,
+        default: () => []
+      },
       tags: {
         type: Array,
         default: () => []
@@ -76,9 +86,10 @@
 
     data () {
       return {
+        defaultTags: [],
+        currentTags: [],
         newTag: '',
         autoTags: [],
-        autoWidth: 0,
         isMatch: true,
         active: false
       }
@@ -86,23 +97,24 @@
 
     computed: {
       tagMax: function () {
-        console.log('tagMax', this.limit, this.tags.length)
-        return this.limit <= this.tags.length;
-      },
-      tips: function() {
-        if (this.limit <= this.tags.length) {
-          return `最多输入${this.limit}个标签`;
-        } else {
-          return this.placeholder;
-        }
+        return this.limit <= this.currentTags.length;
       },
       tagsVal: function(){
-        return JSON.stringify(this.tags);
+        var postTags = this.currentTags.map(function(tag){
+          tag = Object.assign({}, tag);
+          delete tag.checked;
+          return tag;
+        })
+        return JSON.stringify(postTags);
       }
     },
 
     created() {
       let self = this;
+
+      self.currentTags = self.tags.slice();
+      self.defaultTags =  (self.tags && self.tags.length) ? self.tags.slice() : self.hotTags;
+
       // 点击页面其他位置，关闭标签切换浮层
       $(document).on('click.inputTag', (ev)=>{
         if ($(ev.target).parents('.ui-inputtag').length === 0) {
@@ -114,10 +126,7 @@
     watch: {
       autoTags: function () {
         let self = this;
-        let widthTotal = this.$el.offsetWidth;
-        let left = this.$el.querySelector('.ui-inputtag-new').offsetLeft;
-        this.autoWidth = widthTotal - left;
-        this.isMatch = this.autoTags.some(function(item){
+        self.isMatch = self.autoTags.some(function(item){
           return item.name === self.newTag;
         })
       },
@@ -131,27 +140,50 @@
     },
 
     methods: {
-      focusNewTag () {
-        if (this.readOnly) { return }
-        this.$el.querySelector('input').focus()
-        this.active = true;
-        console.log('active true')
+      toggleTag(tag, index) {
+        if (tag.checked) {
+          this.$set(this.defaultTags[index], 'checked', false);
+          this.removeCurrentTagByVal(tag);
+        } else if (!this.tagMax) {
+          this.$set(this.defaultTags[index], 'checked', true);
+          this.addCurrentTag(tag);
+        }
+        console.log('toogle', this, this.defaultTags, this.currentTags)
       },
 
-      addTag (tag, isNew) {
+      addCurrentTag (tag) {
+        tag = Object.assign({}, tag);
+        if (this.validateExists(tag) || this.tagMax) return;
+        this.currentTags.push(tag);
+      },
+
+      removeCurrentTagByVal(tag) {
+        let removeIndex = this.currentTags.findIndex(function(val){
+          return val.id === tag.id;
+        });
+
+        this.currentTags.splice(removeIndex, 1);
+      },
+
+      addTagItem (tag, isNew) {
+        if (this.validateExists(tag) || this.tagMax) return;
+
         if (isNew) {
           tag = {
-            name: this.newTag
+            name: this.newTag,
+            checked: true
           }
+        } else {
+          tag.checked = true;
         }
-
-        if (tag && !this.validateExists(tag) && !this.tagMax) {
-          this.tags.push(tag)
-          this.tagChange()
-        }
+        tag = Object.assign({}, tag);
+        
+        this.defaultTags.push(tag);
+        this.currentTags.push(tag);
         this.newTag = ''
         this.autoTags = [];
       },
+      
 
       addTagActive () {
         var tag;
@@ -175,7 +207,7 @@
       },
 
       validateExists (tag) {
-        return this.tags.some(function(tagObj){
+        return this.currentTags.some(function(tagObj){
           return tagObj.name === tag.name;
         })
       },
@@ -259,12 +291,9 @@
 <style>
 .ui-inputtag {
   position: relative;
-  background-color: #fff;
-  border: 1px solid #ced4da;
-  border-radius: 0.25rem;
-  padding: 4px;
-  cursor: text;
-  text-align: left;
+  display: flex;
+  flex-wrap: wrap;
+  margin-bottom: -5px;
 
   &:focus {
     color: #495057;
@@ -274,39 +303,41 @@
   }
 }
 
+.ui-inputtag-addition {
+  padding-right: 20px;
+}
+
 .ui-inputtag-item {
+  display: inline-block;
   background-color: #f6f9fa;
-  border-radius: 2px;
+  border-radius: 13px;
   border: 1px solid #c8d6df;
   color: #638421;
-  display: inline-block;
   font-size: 13px;
-  font-weight: 400;
-  margin-right: 4px;
-  padding: 3px;
-}
-
-.ui-inputtag-item em {
-  font-style: normal;
-  color: #585858;
-}
-
-.ui-inputtag-item .remove {
+  line-height: 24px;
+  padding: 0 20px;
+  margin-right: 15px;
+  margin-bottom: 15px;
+  vertical-align: middle;
+  text-align: center;
   cursor: pointer;
-  font-weight: bold;
-  color: #638421;
+  transition: all .2s;
 }
 
-.ui-inputtag-item .remove:hover {
-  text-decoration: none;
+.ui-inputtag-item.selected {
+  background: #1e9780;
+  color: #fff !important;
 }
 
-.ui-inputtag-item .remove::before {
-  content: " x";
+.ui-inputtag-search {
+  display: inline-block;
+  position: relative;
+  width: 130px;
+  height: 26px;
 }
 
 .ui-inputtag-new {
-  width: 150px;
+  /* width: 150px;
   background: transparent;
   border: 0;
   color: #777;
@@ -315,13 +346,29 @@
   outline: none;
   padding: 4px;
   padding-left: 0;
+  display: block; */
+    width: 130px;
+    height: 26px;
+    padding: 0 4px;
+    border: 1px solid #e5e5e5;
+    background-color: #f6f6f6;
+    font-size: 14px;
+    line-height: 24px;
+    color: #999;
+    /* position: absolute;
+    left: 0;
+    top: 0; */
+    z-index: 2;
+    -webkit-transition: all .2s;
+    transition: all .2s;
 }
 
 .ui-inputtag-add {
   position: absolute;
-  top: 12px;
-  right: 10px;
+  top: 8px;
+  right: 6px;
   border: none;
+  color: #c1c1c1;
   background: transparent;
 }
 
@@ -329,12 +376,13 @@
   cursor: default;
 }
 
-.ui-inputtag-search {
+.ui-inputtag-result {
   display: flex;
   flex-direction: column;
   position: absolute;
   top: 100%;
-  right: 0;
+  left: 0;
+  min-width: 100%;
   background: #fff;
   border: 1px solid #e7eaf1;
   border-top: none;
@@ -350,5 +398,13 @@
 .ui-inputtag-search a.active,
 .ui-inputtag-search a:active {
   background: #f4f8fb;
+}
+
+.ui-inputtag-mark {
+  display: inline-block;
+  font-size: 12px;
+  line-height: 26px;
+  color: #999;
+  margin-left: 15px;
 }
 </style>
